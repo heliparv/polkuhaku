c0db80246640b19d1af3f2d5164da5be49b25945
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2022-12-29 17:32:52 +0200

    Linttaa ja refaktoroi polkuhaku

--- src/labyrintti.py
+++ src/labyrintti.py
@@ -1,34 +1,37 @@
-from copy import deepcopy
 
 class Labyrintti:
-    '''Luokka joka käsittelee annettua tekstimuotoista labyrinttia ja muodostaa
-    siitä verkkomuotoisen esityksen.
+    '''Luokka joka käsittelee annetun tekstimuotoisen labyrintin ja muodostaa
+    siitä verkkomuotoisen esityksen. Tallentaa myös alkuperäisen ASCII-grafiikan.
 
-    Labyrintin muodon sääntöjä: Reunat ovat aina seinää, lukuun ottamatta alku-
-    ja loppupisteitä, jotka ovat aina labyrintin reunoilla. Alku- tai loppupisteet
-    eivät voi olla kulmissa. Labyrintti koostuu vain seinästä ja lattiasta.
-    Labyrintissa voi liikkua pystyyn ja vaakaan jos vierekkäiset palat ovat lattiaa.
-    Vinottain voi liikkua jos vinoruutu on lattiaa ja vähintään toinen lähtöruudun
-    ja vinoruudun vieressä olevista ruuduista on lattiaa. Vinosiirtymää käsitellään
-    saman pituisena kuin sivulle tai pystysuunnassa siirtymistä
+    Luokan ymmärtämät labyrintit
+    - Reunat ovat seiniä
+    - Labyrintin alku- ja loppusolmu ('A' ja 'B') ovat seinillä, eivät kulmissa
+    - Labyrintin sisus on seinää '#' tai lattiaa ' '
+    - Labyrintissa liikutaan pystyyn, vaakaan ja vinottain, vinottainen liike
+      lasketaan saman pituisena kuin muut siirtymät
+    - Vinottain voi liikkua jos vinoruutu on lattiaa ja vähintään toinen lähtöruudun
+      ja vinoruudun vieressä olevista ruuduista on lattiaa
 
     Luokan muuttujat:
     grafiikka: tallentaa ASCII-grafiikan
-    labyrintti: tallentaa labyrintin verkkoesityksen sanakirjana
-    kaaret: tallentaa labyrintista löytyvien kaarien määrän, tätä käytetään
-    mitattaessa algoritmien aikavaativuutta
+
+    labyrintti: tallentaa labyrintin verkkoesityksen sanakirjana, jossa avain on
+    solmun koordinaatti ja arvona lista koordinaatteja solmuille joihin on yhteys
+
+    kaaret: Tallentaa labyrintista löytyvien kaarien määrän, tätä käytetään jos halutaan
+    verrata algoritmin tekemiä käsittelykierroksia annetun syötteen kokoon
     '''
-    def __init__(self, grafiikka = 0):
-        '''alustusfunktio. Mikäli grafiikkaa ei ole annettu, käyttää
+
+    def __init__(self, grafiikka = ''):
+        '''Alustetaan luokan käyttämät muuttujat. Mikäli grafiikkaa ei ole annettu, käyttää
         esimerkkigrafiikkaa. Jos grafiikka on annettu, asettaa sen labyrintin grafiikaksi
-        Käyttää lue_labyrintti -funktiota luodakseen grafiikasta sanakirjaesityksen, joka
-        kuvaa joka ruudulle minne siitä on pääsy. Muuttujaan ratkaisu sijoitetaan myöhemmin
-        ratkaisun graafinen esitys.'''
-        if grafiikka == 0:
+        Käyttää lue_labyrintti -funktiota luodakseen grafiikasta sanakirjaesityksen ja
+        laske_kaaret funktiota käytetään laskemaan kaaret'''
+        if grafiikka == '':
             self.grafiikka = ['#A##########',
                               '# #  #     #',
                               '# #     #  #',
-                              '#     ##   #',
+                              '#   ####   #',
                               '###    ##  #',
                               '#    ###  ##',
                               '#      #   #',
@@ -38,10 +41,11 @@ class Labyrintti:
         self.labyrintti = {}
         self.kaaret = 0
         self.lue_labyrintti()
+        self.laske_kaaret()
 
     def lue_labyrintti(self):
         '''lue_labyrintti: Tekee labyrintin grafiikasta sanakirjaesityksen käyttämällä
-        lue_pystyreunat-, lue_vaakareunat- ja lue_sisus -funktioita.'''
+        lue_pystyreunat-, lue_vaakareunat-, lue_sisus ja poista_seinasolmut -funktioita.'''
         self.luo_solmut()
         self.lue_pystyreunat()
         self.lue_vaakareunat()
@@ -49,80 +53,77 @@ class Labyrintti:
         self.poista_seinasolmut()
 
     def luo_solmut(self):
-        '''luo_solmut: alustaa kaikki labyrintin sisällä olevat solmut, jottei niitä tarvitse
-        luoda myöhemmin ja tarkistaa luennan yhteydessä ovatko jo olemassa.'''
+        '''luo_solmut: alustaa kaikki labyrintin sisällä olevat solmut, jottei luennan
+        yhteydessä tarvitse tarkistaa onko solmu olemassa vai ei.'''
         for i in range(1, len(self.grafiikka)-1):
             for j in range(1, len(self.grafiikka[0])-1):
                 self.labyrintti[(i,j)] = []
 
     def lue_pystyreunat(self):
-        ''''lue_pystyreunat: funktio lukee annetun grafiikan pystyreunat etsien alku- tai
-        loppupistettä, jotka merkitsee labyrintin solmuesitykseen. Ei tarkastele
-        nurkkapaloja.
+        ''''Syöttää kasittele_pystyreuna funktiolle molemmat pystyreunat'''
 
-        Tarkistaa millä reunalla alku- ja loppusolmut ovat ja merkitsee siirtymän suoraan
-        kohti labyrinttia. Sitten tarkistaa voiko tehdä vinottaisen liikkeen.
+        self.kasittele_pystyreuna(0, 1)
+        self.kasittele_pystyreuna(len(self.grafiikka[0])-1, -1)
 
-        Funktio on täysin turha jos alku ja loppu ovat aina samoissa nurkissa,
-        kuten testilabyrintissa'''
+    def kasittele_pystyreuna(self, reuna, suunta):
+        '''Funktio käsittelee annetun pystyreunan etsien alku- tai loppusolmua. Jos
+        löytää alun tai lopun, se lisätään verkkoon ja sille lisätään siirtymä
+        labyrinttiin sekä tarkistetaan voidaanko tehdä vinottainen siirtymä.
+        Nurkkapaloja ei käsitellä
 
-        for reuna in [0, len(self.grafiikka[0])-1]:
-            for i in range(1, len(self.grafiikka)-1):
-                if self.grafiikka[i][reuna] in ['A', 'B']:
-                    self.labyrintti[(i, reuna)] = []
-                    if reuna == 0:
-                        ero = 1
+        Funktiolle annettu arvo 'reuna' kertoo kummasta reunasta on kyse
+
+        Annettua arvo 'suunta' kertoo mihin suuntaan siirrytään kohti keskustaa'''
+
+        for i in range(1, len(self.grafiikka)-1):
+            if self.grafiikka[i][reuna] in ['A', 'B']:
+                self.labyrintti[(i, reuna)] = []
+                if self.grafiikka[i][reuna+suunta] == ' ':
+                    self.labyrintti[(i,reuna)].append((i, reuna+suunta))
+                    self.labyrintti[(i,reuna+suunta)].append((i,reuna))
+                    if self.grafiikka[i-1][reuna+suunta] == ' ':
+                        self.labyrintti[(i, reuna)].append((i-1,reuna+suunta))
+                        self.labyrintti[(i-1,reuna+suunta)].append((i,reuna))
+                    if self.grafiikka[i+1][reuna+suunta] == ' ':
+                        self.labyrintti[(i,reuna)].append((i+1,reuna+suunta))
+                        self.labyrintti[(i+1,reuna+suunta)].append((i,reuna))
+                    if self.grafiikka[i][reuna] == 'A':
+                        self.labyrintti['alku'] = (i,reuna)
                     else:
-                        ero = -1
-                    if self.grafiikka[i][reuna+ero] == ' ':
-                        self.labyrintti[(i,reuna)].append((i, reuna+ero))
-                        self.labyrintti[(i,reuna+ero)].append((i,reuna))
-                        self.kaaret += 1
-                        if self.grafiikka[i-1][reuna+ero] == ' ':
-                            self.labyrintti[(i, reuna)].append((i-1,reuna+ero))
-                            self.labyrintti[(i-1,reuna+ero)].append((i,reuna))
-                            self.kaaret += 1
-                        if self.grafiikka[i+1][reuna+ero] == ' ':
-                            self.labyrintti[(i,reuna)].append((i+1,reuna+ero))
-                            self.labyrintti[(i+1,reuna+ero)].append((i,reuna))
-                            self.kaaret += 1
-                        if self.grafiikka[i][reuna] == 'A':
-                            self.labyrintti['alku'] = (i,reuna)
-                        else:
-                            self.labyrintti['loppu'] = (i,reuna)
+                        self.labyrintti['loppu'] = (i,reuna)
 
     def lue_vaakareunat(self):
-        '''lue_vaakareunat: funktio lukee annetun grafiikan vaakareunat etsien alku- tai
-        loppupistettä, jonka merkitsee labyrintin solmuesitykseen. Ei tarkastele
-        nurkkapaloja.
+        '''Syöttää kasittele_vaakareuna funktiolle molemmat vaakareunat'''
 
-        Tarkistaa millä reunalla alku- ja loppusolmut ovat ja merkitsee siirtymän suoraan
-        kohti labyrinttia. Sitten tarkistaa voiko tehdä vinottaisen liikkeen.
+        self.kasittele_vaakareuna(0, 1)
+        self.kasittele_vaakareuna(len(self.grafiikka)-1, -1)
 
-        Funktio on täysin turha jos alku ja loppu ovat aina samoissa nurkissa,
-        kuten testilabyrintissa'''
+    def kasittele_vaakareuna(self, reuna, suunta):
+        '''Funktio käsittelee annetun vaakareunan etsien alku- tai loppusolmua. Jos
+        löytää alun tai lopun, se lisätään verkkoon ja sille lisätään siirtymä
+        labyrinttiin sekä tarkistetaan voidaanko tehdä vinottainen siirtymä.
+        Nurkkapaloja ei käsitellä
 
-        for reuna in [0, len(self.grafiikka)-1]:
-            for j in range(1, len(self.grafiikka[0])-1):
-                if self.grafiikka[reuna][j] in ['A', 'B']:
-                    self.labyrintti[(reuna,j)] = []
-                    if reuna == 0:
-                        ero = 1
-                    else:
-                        ero = -1
-                    if self.grafiikka[reuna+ero][j] == ' ':
-                        self.labyrintti[(reuna,j)].append((reuna+ero,j))
-                        self.labyrintti[(reuna+ero,j)].append((reuna,j))
-                    if self.grafiikka[reuna+ero][j-1] == ' ':
-                        self.labyrintti[(reuna,j)].append((reuna+ero,j-1))
-                        self.labyrintti[(reuna+ero,j-1)].append((reuna,j))
-                    if self.grafiikka[reuna+ero][j-1] == ' ':
-                        self.labyrintti[(reuna,j)].append((reuna+ero,j-1))
-                        self.labyrintti[(reuna+ero,j-1)].append((reuna,j))
-                    if self.grafiikka[reuna][j] == 'A':
-                        self.labyrintti['alku'] = (reuna,j)
-                    else:
-                        self.labyrintti['loppu'] = (reuna,j)
+        Funktiolle annettu arvo 'reuna' kertoo kummasta reunasta on kyse
+
+        Annettua arvo 'suunta' kertoo mihin suuntaan siirrytään kohti keskustaa'''
+
+        for j in range(1, len(self.grafiikka[0])-1):
+            if self.grafiikka[reuna][j] in ['A', 'B']:
+                self.labyrintti[(reuna,j)] = []
+                if self.grafiikka[reuna+suunta][j] == ' ':
+                    self.labyrintti[(reuna,j)].append((reuna+suunta,j))
+                    self.labyrintti[(reuna+suunta,j)].append((reuna,j))
+                if self.grafiikka[reuna+suunta][j-1] == ' ':
+                    self.labyrintti[(reuna,j)].append((reuna+suunta,j-1))
+                    self.labyrintti[(reuna+suunta,j-1)].append((reuna,j))
+                if self.grafiikka[reuna+suunta][j-1] == ' ':
+                    self.labyrintti[(reuna,j)].append((reuna+suunta,j-1))
+                    self.labyrintti[(reuna+suunta,j-1)].append((reuna,j))
+                if self.grafiikka[reuna][j] == 'A':
+                    self.labyrintti['alku'] = (reuna,j)
+                else:
+                    self.labyrintti['loppu'] = (reuna,j)
 
     def lue_sisus(self):
         '''lue_sisus: Funktio käy läpi annetun grafiikan muut kuin reunapalat ja merkitsee
@@ -134,37 +135,31 @@ class Labyrintti:
                 if self.grafiikka[i+1][j+1] == ' ' and (self.grafiikka[i+1][j] == ' ' or self.grafiikka[i][j+1] == ' '):
                     self.labyrintti[(i,j)].append((i+1,j+1))
                     self.labyrintti[(i+1,j+1)].append((i,j))
-                    self.kaaret += 1
                 if self.grafiikka[i-1][j+1] == ' ' and (self.grafiikka[i-1][j] == ' ' or self.grafiikka[i][j+1] == ' '):
                     self.labyrintti[(i,j)].append((i-1,j+1))
                     self.labyrintti[(i-1,j+1)].append((i,j))
-                    self.kaaret += 1
                 if self.grafiikka[i+1][j] == ' ':
                     self.labyrintti[(i,j)].append((i+1,j))
                     self.labyrintti[(i+1,j)].append((i,j))
-                    self.kaaret += 1
                 if self.grafiikka[i][j+1] == ' ':
                     self.labyrintti[(i,j)].append((i,j+1))
                     self.labyrintti[(i,j+1)].append((i,j))
-                    self.kaaret += 1
 
     def poista_seinasolmut(self):
         '''Käy läpi labyrintin verkkoesityksen ja poistaa solmut, joista ei
         ole yhteyksiä muihin solmuihin. Tätä käytetään poistamaan alussa
         turhaan luodut solmut, jotka esittävät seiniä. Näin saadaan tarkka
-        mitta algoritmille syötetystä n:stä.'''
+        mitta algoritmille syötetystä solmujen määrästä tarvittaessa.'''
+
         for i in range(1, len(self.grafiikka)-1):
             for j in range(1, len(self.grafiikka[0])-1):
                 if self.labyrintti[(i,j)] == []:
                     del self.labyrintti[(i,j)]
 
-    def tulosta_grafiikka(self):
-        '''Tulostaa grafiikan riveittäin, jotta se näyttää labyrintilta'''
-        for rivi in self.grafiikka:
-            print(rivi)
-
-    def leveys(self):
-        return len(self.grafiikka[0])
-
-    def korkeus(self):
-        return len(self.grafiikka)
+    def laske_kaaret(self):
+        '''Laskee labyrintissa esiintyvät kaaret, eli mahdolliset siirtymät
+        lattiaruudusta toiseen. Summa jaetaan kahdella, koska ajatellaan yhteyksien
+        lattiaruutujen välillä olevan suuntaamattomia kaaria'''
+        for kaaret in self.labyrintti:
+            self.kaaret += len(kaaret)
+        self.kaaret = int(self.kaaret/2)
