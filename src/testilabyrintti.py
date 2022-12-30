ae08299079cccaa0eb4aa777eef087275317ad21
Heli Parviainen <heli.s.parviainen@helsinki.fi>
2022-12-30 12:36:20 +0200

    Linttaa ja refaktoroi testilabyrintti

--- src/testilabyrintti.py
+++ src/testilabyrintti.py
@@ -6,28 +6,27 @@ class Testilabyrintti:
     Luokassa on kolme erilaista vaihtoehtoa labyrinttityypille, kustakin vaihtoehdosta
     on grafiikka, jota toistamalla saadaan aikaan labyrintti. Labyrintit ovat
     yksinkertaisia ja toisteisia, mutta toimivat riittävän hyvin
-    
-    Luokan muuttujat:
-    labyrintti = sisältää ASCII-muotoisen labyrinttigrafiikan'''
+
+    Luokan muuttuja 'labyrintti' sisältää ASCII-muotoisen labyrinttigrafiikan'''
 
     def __init__(self, leveys = 1, korkeus = 1, tiheys = 'harva'):
-        '''Käyttää luo_labyrintti funktiota luodakseen
-        labyrintin annettujen leveyden, korkeuden ja
-        harva, tiheä vai siksak määrityksen perusteella.
-        Luo harvan labyrintin jos käyttäjän syöte ei vastaa mitään
-        labyrinttityyppiä tai syötettä ei ole.'''
+        '''Käyttää luo_labyrintti funktiota luodakseen labyrintin annettujen leveyden,
+        korkeuden ja harva, tiheä vai siksak määrityksen perusteella. Luo harvan
+        labyrintin jos käyttäjän syöte ei vastaa mitään labyrinttityyppiä tai syötettä
+        ei ole.'''
+
         self.labyrintti = []
         if leveys > 0 and korkeus > 0:
             if tiheys == 'siksak':
-                self.luo_labyrintti(leveys, korkeus, self.siksak())
+                self.luo_labyrintti(leveys, korkeus, siksak())
             elif tiheys == 'tiheä':
-                self.luo_labyrintti(leveys, korkeus, self.tihea())
+                self.luo_labyrintti(leveys, korkeus, tihea())
             else:
-                self.luo_labyrintti(leveys, korkeus, self.harva())
+                self.luo_labyrintti(leveys, korkeus, harva())
         else:
             self.luo_labyrintti(1, 1, self.harva())
 
-    def luo_labyrintti(self, leveys: int, korkeus: int, metodi):
+    def luo_labyrintti(self, leveys: int, korkeus: int, kuvio):
         '''Luo labyrintin käyttäen toistokuviota.
         Toistokuvioita voi laittaa vierekkäin ja päällekäin
         halutun verran ja alku- ja loppusolmu tulevat aina
@@ -36,7 +35,7 @@ class Testilabyrintti:
         Funktiolle annetut muuttujat leveys ja korkeus
         kertovat toistojen lukumäärän'''
 
-        toistokuvio = metodi
+        toistokuvio = kuvio
         toiston_leveys = len(toistokuvio[0])
         self.labyrintti = []
         for rivi in toistokuvio:
@@ -45,51 +44,48 @@ class Testilabyrintti:
         self.labyrintti.insert(0, '#A'+'#'*toiston_leveys*leveys)
         self.labyrintti.append('#'*toiston_leveys*leveys+'B#')
 
-    def harva(self):
-        ''''Palauttaa labyrintin luontia varten harvan
-        toistokuvion.'''
+def harva():
+    ''''Palauttaa labyrintin luontia varten harvan
+    toistokuvion.'''
 
-        return ['               ',
-                '##            #',
-                '     #####     ',
-                '               ',
-                '#      #    ###',
-                '       #       ',
-                '  #    #       ',
-                '  #            ',
-                '  #            ',
-                '        ###    ']
+    return ['               ',
+            '##            #',
+            '     #####     ',
+            '               ',
+            '#      #    ###',
+            '       #       ',
+            '  #    #       ',
+            '  #            ',
+            '  #            ',
+            '        ###    ']
 
-    def tihea(self):
-        ''''Palauttaa labyrintin luontia varten tiheän
-        toistokuvion.'''
+def tihea():
+    ''''Palauttaa labyrintin luontia varten tiheän
+    toistokuvion.'''
 
-        return [' #       ######',
-                ' # #####      #',
-                '   #    ##### #',
-                '#### ##   #   #',
-                '       #  # #  ',
-                '###### #### ###',
-                '            ## ',
-                '# #########  # ',
-                '#        ##### ',
-                '########       ']
+    return [' #       ######',
+            ' # #####      #',
+            '   #    ##### #',
+            '#### ##   #   #',
+            '       #  # #  ',
+            '###### #### ###',
+            '            ## ',
+            '# #########  # ',
+            '#        ##### ',
+            '########       ']
 
-    def siksak(self):
-        '''Palauttaa labyrintin luontia varten toistokuvion,
-        jolla voidaan luoda pystysuuntaista siksakkia sisältävä
-        labyrintti.'''
+def siksak():
+    '''Palauttaa labyrintin luontia varten toistokuvion,
+    jolla voidaan luoda pystysuuntaista siksakkia sisältävä
+    labyrintti.'''
 
-        return ['   #     #     ',
-                '   #  #  #  #  ',
-                '   #  #  #  #  ',
-                '#  #  #  #  #  ',
-                '#  #  #  #  #  ',
-                '#  #  #     #  ',
-                '#     #  #  #  ',
-                '#     #  #  #  ',
-                '#  #  #  #  #  ',
-                '#  #     #     ',]
-
-    def anna_labyrintti(self):
-        return self.labyrintti
+    return ['   #     #     ',
+            '   #  #  #  #  ',
+            '   #  #  #  #  ',
+            '#  #  #  #  #  ',
+            '#  #  #  #  #  ',
+            '#  #  #     #  ',
+            '#     #  #  #  ',
+            '#     #  #  #  ',
+            '#  #  #  #  #  ',
+            '#  #     #     ',]
